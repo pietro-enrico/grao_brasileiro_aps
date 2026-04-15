397458aea51673532b2e1ed1229c771e9300a32d
José Henrique <113061751+HenriqueLimaJHLA@users.noreply.github.com>
2026-04-15 15:24:16 -0300

    feat: início módulo de registro, focando mais no frontend da tela.

---
+++ src/main/java/register/Register.java
@@ -0,0 +1,20 @@
+package register;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class Register extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Register.class.getResource("register-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
+        stage.setTitle("Cadastro de Usuário - Grão Brasileiro");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
