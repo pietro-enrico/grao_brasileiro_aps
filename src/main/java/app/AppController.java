58231f6d222b916c36fb65729295740922329df6
José Henrique <113061751+HenriqueLimaJHLA@users.noreply.github.com>
2026-04-15 23:03:24 -0300

    feat: adicionado id nos inputs e capturados os seus valores para validações;

--- src/main/java/app/AppController.java
+++ src/main/java/app/AppController.java
@@ -3,12 +3,4 @@ package app;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class AppController {
-    @FXML
-    private Label welcomeText;
-
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
-}
+public class AppController { }
