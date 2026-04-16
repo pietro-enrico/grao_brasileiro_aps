178f6bdf5f85f8f7f02081728b4c776b43a3d70f
José Henrique <113061751+HenriqueLimaJHLA@users.noreply.github.com>
2026-04-16 20:09:17 -0300

    feat: conexão com banco de dados realizada.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module app.graobrasileiroaps {
 
     requires org.kordamp.bootstrapfx.core;
     requires java.desktop;
+    requires java.sql;
 
     opens app to javafx.fxml;
     opens register to javafx.fxml;
