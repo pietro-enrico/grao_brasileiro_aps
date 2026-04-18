591455f3635dbb2ea13123a2db54310eb8f224de
Pietro Enrico <pietroenrico@pangeiax.com>
2026-04-18 15:34:02 -0300

    Refatoração no design das telas

--- src/main/java/donate/Donate.java
+++ src/main/java/donate/Donate.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class Donate extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Donate.class.getResource("water-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Donate.class.getResource("donate-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setTitle("Doação - Grão Brasileiro");
         stage.setScene(scene);
