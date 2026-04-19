b28b1efe59f600bc12947f7092a82f76514eafa0
José Henrique <113061751+HenriqueLimaJHLA@users.noreply.github.com>
2026-04-19 17:27:29 -0300

    feat: término da modelagem do banco de dados no sqlite.

--- src/main/java/config/database/db-grao-brasileiro.sql
+++ src/main/java/config/database/db-grao-brasileiro.sql
@@ -1,46 +1,54 @@
 CREATE TABLE IF NOT EXISTS "User" (
-	"id_user" INTEGER NOT NULL UNIQUE,
-	"full_name" VARCHAR NOT NULL,
-	"email" VARCHAR NOT NULL UNIQUE,
-	"cpf" VARCHAR NOT NULL UNIQUE,
-	"password" VARCHAR NOT NULL,
-	"createdAt" TIMESTAMP NOT NULL,
-	"isVoluntary" BOOLEAN DEFAULT 0,
-	PRIMARY KEY("id_user")
+                                      "id_user" INTEGER NOT NULL UNIQUE,
+                                      "full_name" VARCHAR NOT NULL,
+                                      "email" VARCHAR NOT NULL UNIQUE,
+                                      "cpf" VARCHAR NOT NULL UNIQUE,
+                                      "password" VARCHAR NOT NULL,
+                                      "createdAt" TIMESTAMP NOT NULL,
+                                      "isVoluntary" BOOLEAN DEFAULT 0,
+                                      "fk_id_company" VARCHAR,
+                                      PRIMARY KEY("id_user"),
+                                      FOREIGN KEY ("fk_id_company") REFERENCES "CompanyOng"("cnpj")
+                                          ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 
 CREATE TABLE IF NOT EXISTS "Donate" (
-	"id_donate" INTEGER NOT NULL UNIQUE,
-	"category" VARCHAR NOT NULL,
-	"sub_category" VARCHAR,
-	"quantity" INTEGER,
-	"value_payment" REAL,
-	"method_value_payment" VARCHAR,
-	PRIMARY KEY("id_donate")
-);
-
-CREATE TABLE IF NOT EXISTS "Storage" (
-	"id_storage" INTEGER NOT NULL UNIQUE,
-	"volume_kg" REAL NOT NULL DEFAULT 0,
-	"volume_li" REAL NOT NULL DEFAULT 0,
-	"data_kg" TIMESTAMP NOT NULL,
-	"data_li" TIMESTAMP NOT NULL,
-	PRIMARY KEY("id_storage")
+                                        "id_donate" INTEGER NOT NULL UNIQUE,
+                                        "category" VARCHAR NOT NULL,
+                                        "sub_category" VARCHAR,
+                                        "quantity" REAL,
+                                        "unity" VARCHAR,
+                                        "value_payment" REAL,
+                                        "method_value_payment" VARCHAR,
+                                        "donation_was_sent" BOOLEAN DEFAULT 0,
+                                        "date_donation" TIMESTAMP NOT NULL,
+                                        "fk_id_user" INTEGER NOT NULL,
+                                        PRIMARY KEY("id_donate"),
+                                        FOREIGN KEY ("fk_id_user") REFERENCES "User"("id_user")
+                                            ON UPDATE NO ACTION ON DELETE NO ACTION,
+                                        FOREIGN KEY ("id_donate") REFERENCES "Address"("fk_id_donate")
+                                            ON UPDATE NO ACTION ON DELETE NO ACTION
 );
 
 CREATE TABLE IF NOT EXISTS "CompanyOng" (
-	"cnpj" VARCHAR NOT NULL UNIQUE,
-	"name" VARCHAR NOT NULL,
-	PRIMARY KEY("cnpj")
+                                            "cnpj" VARCHAR NOT NULL UNIQUE,
+                                            "name" VARCHAR NOT NULL,
+                                            PRIMARY KEY("cnpj")
 );
 
 CREATE TABLE IF NOT EXISTS "Address" (
-	"id_address" INTEGER NOT NULL UNIQUE,
-	"street" VARCHAR NOT NULL,
-	"number" INTEGER NOT NULL,
-	"bairro" VARCHAR NOT NULL,
-	"cep" INTEGER NOT NULL,
-	"cidade" VARCHAR NOT NULL DEFAULT 'São Paulo',
-	"estado" VARCHAR NOT NULL DEFAULT 'SP',
-	PRIMARY KEY("id_address")
+                                         "id_address" INTEGER NOT NULL UNIQUE,
+                                         "street" VARCHAR NOT NULL,
+                                         "number" INTEGER NOT NULL,
+                                         "bairro" VARCHAR NOT NULL,
+                                         "cep" INTEGER NOT NULL,
+                                         "cidade" VARCHAR NOT NULL DEFAULT 'São Paulo',
+                                         "estado" VARCHAR NOT NULL DEFAULT 'SP',
+                                         "fk_id_company" VARCHAR NOT NULL,
+                                         "fk_id_donate" INTEGER,
+                                         PRIMARY KEY("id_address"),
+                                         FOREIGN KEY ("fk_id_company") REFERENCES "CompanyOng"("cnpj")
+                                             ON UPDATE NO ACTION ON DELETE NO ACTION
 );
+
+INSERT INTO "CompanyOng" VALUES ('73.884.979/0001-81', 'Grão Brasileiro');
